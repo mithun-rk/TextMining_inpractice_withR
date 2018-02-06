d69cdd5627080492e76443f153c655ac7ce91660
Mithun Radhakrishnan <mithunradhakrishnan@hotmail.com>
2018-02-06 16:44:50 +0530

    first commit

---
+++ chap2.R
@@ -0,0 +1,13 @@
+# Text Mining in Practice in R
+# Mithun Radhakrishnan 
+# mithunradhakrishnan@hotmail.com
+# Chapter 2 Text mining basics
+
+# Set Options 
+options(stringsAsFactors = FALSE)
+Sys.setlocale('LC_ALL','C')
+
+# Calling libraries for data manipulation
+library(stringi)
+library(stringr)
+library(qdap)
